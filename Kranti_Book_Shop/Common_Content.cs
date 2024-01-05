c886401473c3aaad7be39edec62f7c54767313c0
Pranali Thorat <100304254+Pranali34@users.noreply.github.com>
2024-01-05 20:57:38 +0530

    Upload

---
+++ Kranti_Book_Shop/Common_Content.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kranti_Book_Shop
+{
+    class Common_Content
+    {
+        public static String Log_UName = " ";
+    }
+
+
+}
